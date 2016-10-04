4409b156498fd936e29d15d9722cb57d96356a0c
Tomasz Madej <tomasz.madej1990@gmail.com>
2016-10-04 11:28:01 +0200

    Home - poprawka dla skryptu dla box-services

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     homeSliderInit();
     fixedMenu();
+    if($(".homepage .services").length>0){    
+        var brake_array = {};
+        brake_array["1200"] = 4;
+        brake_array["992"] = 3;
+        brake_array["768"] = 2;    
+        brake_array["469"] = 1; 
+        rowModuleBreaker(".homepage .services","box-services",brake_array,"first","last");
+    }
 });
 
 function homeSliderInit(){
@@ -32,7 +40,69 @@ function fixedMenu(){
             }
         }
     });
+}
+function rowModuleBreaker(parent,css_selector,break_array,class_first_row,class_last_row){
+    var oldCols=0;
+    $(window).resize(function(){
+        var cols=0;
+        var lastBrake=0;
+        for(var windowBreak in break_array){
+            if(window.innerWidth<=parseInt(windowBreak)){
+                cols=break_array[windowBreak];
+                break;
+            }
+            lastBreak=windowBreak;
+        }
 
+        if(cols==0){
+            cols=break_array[lastBreak];
+        }
+        if(oldCols==0||oldCols!=cols){
+            $(parent).each(function(j,selector){
+            
+            var old=oldCols;
+            oldCols=cols;
+            var inc=-1;
+            var row=[];
+            var length=$(selector).find(css_selector).length;
+            $(selector).find(css_selector).each(function(i,e){
+                if(i%cols==0){
+                    inc++;
+                    row=[];
+                }
+                row.push($(e).parent().clone(true));
+                $(e).parent().parent().addClass("old_remove");
+                if(i%cols==cols-1){
+                    var rowContainer=$("<div class='row'></div>");
+                    if(inc==0){
+                        rowContainer.addClass(class_first_row);
+                    }
+                    if(i==length-1){
+                        rowContainer.addClass(class_last_row);
+                    }
+                    rowContainer.append(row);
+                    $(e).parent().parent().before(rowContainer);
+                    
+                }
+
+                if(i%cols<cols-1 && i==length-1){
+                        var rowContainer=$("<div class='row'></div>");
+                        if(inc==0){
+                            rowContainer.addClass(class_first_row);
+                        }
+                        rowContainer.addClass(class_last_row);
+                        rowContainer.append(row);
+                        $(e).parent().parent().before(rowContainer);
+                }
+                if($(e).parent().parent().hasClass("hs_cos_wrapper_type_custom_widget")){
+                    $(e).parent().parent().remove();
+                }
+            });
+            $(".old_remove").remove();
+            });
+        }
+    });
+    $(window).resize();
 }
 
     if (window.innerWidth > 767) {
